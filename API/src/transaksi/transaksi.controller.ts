dad2a150853393efda7ea72fa71d13b135c28a7f
Azizteknokrat <aziz_prayoga@teknokrat.ac.id>
2026-04-28 14:00:27 +0700

    perbaiki format dengan merapikan import dan parameter pada transaksi.controller.ts

--- API/src/transaksi/transaksi.controller.ts
+++ API/src/transaksi/transaksi.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { TransaksiService } from './transaksi.service';
 import { CreateTransaksiDto } from './dto/create-transaksi.dto';
 import { UpdateTransaksiDto } from './dto/update-transaksi.dto';
@@ -23,7 +31,10 @@ export class TransaksiController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTransaksiDto: UpdateTransaksiDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateTransaksiDto: UpdateTransaksiDto,
+  ) {
     return this.transaksiService.update(+id, updateTransaksiDto);
   }
 

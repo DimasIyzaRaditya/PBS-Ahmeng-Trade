da8beccf75bd93403cedd47e9fc7b3fc1c3749ff
Azizteknokrat <aziz_prayoga@teknokrat.ac.id>
2026-04-28 14:00:08 +0700

    perbaiki format dengan merapikan import di produk.controller.ts

--- API/src/produk/produk.controller.ts
+++ API/src/produk/produk.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ProdukService } from './produk.service';
 import { CreateProdukDto } from './dto/create-produk.dto';
 import { UpdateProdukDto } from './dto/update-produk.dto';

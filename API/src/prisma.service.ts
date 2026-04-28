9a6e27f63ac64ff75920e5f6bc20e31ab094f996
Azizteknokrat <aziz_prayoga@teknokrat.ac.id>
2026-04-28 13:51:36 +0700

    tambah import dotenv ke dalam prisma.service.ts

--- API/src/prisma.service.ts
+++ API/src/prisma.service.ts
@@ -1,3 +1,5 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '../generated/prisma/index.js';
 import { PrismaPg } from '@prisma/adapter-pg';
+import * as dotenv from 'dotenv';
+

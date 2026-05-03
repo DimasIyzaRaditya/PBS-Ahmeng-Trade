6a5093329b53a46c98e0d18f24b9e246fcfe1ad9
Azizteknokrat <aziz_prayoga@teknokrat.ac.id>
2026-05-03 13:09:26 +0700

    hapus main transaksi

--- API/user/src/main-transaksi.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { TransaksiModule } from './transaksi/transaksi.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(TransaksiModule);
-  await app.listen(3003);
-}
-
-bootstrap();

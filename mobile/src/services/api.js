206c2e93eee378564a957bbc584952c75324e10c
ridhorr <153862510+ridhorr@users.noreply.github.com>
2026-05-04 11:01:16 +0700

    tambah: tambah fungsi register ke api.js

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -7,3 +7,7 @@ export const apiLogin = async (email, password) => {
 export const apiGetProduk = async () => {
   return [];
 };
+
+export const apiRegister = async (nama, email, password) => {
+  return { token: 'dummy-token', user: { nama, email } };
+};

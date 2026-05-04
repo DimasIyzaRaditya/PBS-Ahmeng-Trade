4003328d1335a4a6c045f94463e6a4514b7b6fb9
ridhorr <153862510+ridhorr@users.noreply.github.com>
2026-05-04 10:53:09 +0700

    tambah: tambah fungsi get produk ke api.js

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -3,3 +3,7 @@ const BASE_URL = 'http://localhost:8000/api';
 export const apiLogin = async (email, password) => {
   return { token: 'dummy-token', user: { email } };
 };
+
+export const apiGetProduk = async () => {
+  return [];
+};

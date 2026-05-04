84568e5fea613d0492eb244813965a1e0986f767
ridhorr <153862510+ridhorr@users.noreply.github.com>
2026-05-04 13:48:24 +0700

    menambahkan style

--- mobile/src/screens/auth/RegisterScreen.js
+++ mobile/src/screens/auth/RegisterScreen.js
@@ -3,8 +3,14 @@ import { View, Text, StyleSheet } from 'react-native';
 
 export default function RegisterScreen({ navigation }) {
   return (
-    <View>
-      <Text>Register Screen</Text>
+    <View style={styles.container}>
+      <Text>Daftar Akun</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});

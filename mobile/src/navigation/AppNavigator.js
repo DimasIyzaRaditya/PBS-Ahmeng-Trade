fac30306ad4857c1d0e346ea6fea39856557e408
ridhorr <153862510+ridhorr@users.noreply.github.com>
2026-05-04 11:11:34 +0700

    tambah: tambah import ProdukScreen dan ProfileScreen ke AppNavigator

--- mobile/src/navigation/AppNavigator.js
+++ mobile/src/navigation/AppNavigator.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
+import ProdukScreen from '../screens/ProdukScreen';
+import ProfileScreen from '../screens/ProfileScreen';
 
 const Tab = createBottomTabNavigator();

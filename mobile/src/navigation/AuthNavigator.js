e64727ba839af4be053d6b713e7d04503c859165
ridhorr <153862510+ridhorr@users.noreply.github.com>
2026-05-04 11:08:12 +0700

    tambah: tambah import RegisterScreen ke AuthNavigator

--- mobile/src/navigation/AuthNavigator.js
+++ mobile/src/navigation/AuthNavigator.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from '../screens/auth/LoginScreen';
+import RegisterScreen from '../screens/auth/RegisterScreen';
 
 const Stack = createStackNavigator();

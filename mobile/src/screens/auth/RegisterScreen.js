77217ad972d8f0046ae327f6087ceb02ea6c00ed
ridhorr <153862510+ridhorr@users.noreply.github.com>
2026-05-04 13:47:31 +0700

    menambahkan navigation register

--- mobile/src/screens/auth/RegisterScreen.js
+++ mobile/src/screens/auth/RegisterScreen.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-export default function RegisterScreen() {
+export default function RegisterScreen({ navigation }) {
   return (
     <View>
       <Text>Register Screen</Text>
     </View>
   );
 }
-

921b82ec35d57f85f7d1034b934960741fe963c8
ridhorr <153862510+ridhorr@users.noreply.github.com>
2026-05-04 10:04:43 +0700

    tambah: tambah fungsi logout ke AuthContext

--- mobile/src/context/AuthContext.js
+++ mobile/src/context/AuthContext.js
@@ -8,9 +8,10 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
   const login = (userData) => setUser(userData);
+  const logout = () => setUser(null);
 
   return (
-    <AuthContext.Provider value={{ user, login }}>
+    <AuthContext.Provider value={{ user, login, logout }}>
       {children}
     </AuthContext.Provider>
   );

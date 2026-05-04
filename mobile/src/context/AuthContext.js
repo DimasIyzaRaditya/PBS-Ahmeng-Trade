a974fd546f136df3dceddc56bdfbc191f6c86014
ridhorr <153862510+ridhorr@users.noreply.github.com>
2026-05-04 10:04:21 +0700

    tambah: tambah fungsi login ke AuthContext

--- mobile/src/context/AuthContext.js
+++ mobile/src/context/AuthContext.js
@@ -7,8 +7,10 @@ export const useAuth = () => useContext(AuthContext);
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
+  const login = (userData) => setUser(userData);
+
   return (
-    <AuthContext.Provider value={{ user }}>
+    <AuthContext.Provider value={{ user, login }}>
       {children}
     </AuthContext.Provider>
   );

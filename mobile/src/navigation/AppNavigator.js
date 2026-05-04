aded22f748a471f442a20e158d2fe6289dddbca7
ridhorr <153862510+ridhorr@users.noreply.github.com>
2026-05-04 11:14:50 +0700

    tambah: tambah fungsi AppNavigator dengan tab Produk

--- mobile/src/navigation/AppNavigator.js
+++ mobile/src/navigation/AppNavigator.js
@@ -10,6 +10,7 @@ export default function AppNavigator() {
   return (
     <Tab.Navigator>
       <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Produk" component={ProdukScreen} />
     </Tab.Navigator>
   );
 }

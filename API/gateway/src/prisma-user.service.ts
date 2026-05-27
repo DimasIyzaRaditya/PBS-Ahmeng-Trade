95d48251fd1546e801832683984a3a7cecb087fc
Azizteknokrat <aziz_prayoga@teknokrat.ac.id>
2026-05-27 07:42:28 +0700

    inisialisasi struktur mikroservis (Produk, Transaksi, User) dengan Prisma dan validasi antar-servis.

---
+++ API/gateway/src/prisma-user.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '../generated/prisma/index.js';
+import { PrismaPg } from '@prisma/adapter-pg';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+@Injectable()
+export class PrismaUserService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.USER_DATABASE_URL as string,
+    });
+    super({ adapter });
+  }
+  async onModuleInit() {
+    await this.$connect();
+  }
+}

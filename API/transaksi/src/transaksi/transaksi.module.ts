74887f02156ebbcf88b4e7208c74cadedd06142f
Azizteknokrat <aziz_prayoga@teknokrat.ac.id>
2026-05-27 08:24:29 +0700

    implementasi autentikasi pengguna dan servis CRUD inti dengan utilitas respons JSON terstruktur.

--- API/transaksi/src/transaksi/transaksi.module.ts
+++ API/transaksi/src/transaksi/transaksi.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { TransaksiService } from './transaksi.service';
 import { TransaksiController } from './transaksi.controller';
 import { PrismaService } from '../prisma.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [TransaksiController],
   providers: [TransaksiService, PrismaService],
 })

b37168f11a01a1e7e2953456692b62a1b29a58e3
Azizteknokrat <aziz_prayoga@teknokrat.ac.id>
2026-05-27 07:32:55 +0700

    menghapus file yang tidak di gunakan

--- API/produk/src/main-produk.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { ProdukModule } from './produk/produk.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(ProdukModule);
-  await app.listen(3002);
-}
-
-bootstrap();

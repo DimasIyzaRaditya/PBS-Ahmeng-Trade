cb669b7b813821f405c276066fa23a7777873623
Azizteknokrat <aziz_prayoga@teknokrat.ac.id>
2026-05-27 07:40:20 +0700

    inisialisasi arsitektur mikroservis (Gateway, User, Produk, Transaksi) dengan konfigurasi Prisma terpisah.

---
+++ API/gateway/src/prisma-produk.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '../generated/prisma/index.js';
+import { PrismaPg } from '@prisma/adapter-pg';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+@Injectable()
+export class PrismaProdukService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.PRODUK_DATABASE_URL as string,
+    });
+    super({ adapter });
+  }
+  async onModuleInit() {
+    await this.$connect();
+  }
+}

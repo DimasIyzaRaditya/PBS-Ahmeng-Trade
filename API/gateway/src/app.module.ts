f052a6a0744de35a979981a96659f449fa28f457
Azizteknokrat <aziz_prayoga@teknokrat.ac.id>
2026-05-27 08:15:52 +0700

    implementasi modul autentikasi (JWT guard & login) di gateway, user, dan transaksi.

--- API/gateway/src/app.module.ts
+++ API/gateway/src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ProdukModule } from './produk/produk.module';
 import { TransaksiModule } from './transaksi/transaksi.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, ProdukModule, TransaksiModule],
+  imports: [AuthModule, UserModule, ProdukModule, TransaksiModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+

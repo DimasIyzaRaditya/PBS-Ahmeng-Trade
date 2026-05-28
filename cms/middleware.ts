191da109d1169008141d2853c4e51a72e3c6a5b8
DimasIyzaraditya <dimas_iyza_raditya@teknokrat.ac.id>
2026-05-28 18:40:30 +0700

    buat variable pathname

--- cms/middleware.ts
+++ cms/middleware.ts
@@ -2,4 +2,6 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
 

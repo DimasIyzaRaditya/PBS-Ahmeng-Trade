5657328eafab5d53689485c0f0cff37dd800d2d6
ridhorr <153862510+ridhorr@users.noreply.github.com>
2026-05-28 21:46:23 +0700

    tambah: state token di AuthContext

--- mobile/src/context/AuthContext.js
+++ mobile/src/context/AuthContext.js
@@ -7,12 +7,13 @@ export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
 
   const login = (userData) => setUser(userData);
   const logout = () => setUser(null);
 
   return (
-    <AuthContext.Provider value={{ user, login, logout }}>
+    <AuthContext.Provider value={{ user, token, login, logout }}>
       {children}
     </AuthContext.Provider>
   );

47ecf76aa99860462dc18253ed53a926d2f2285b
Azizteknokrat <aziz_prayoga@teknokrat.ac.id>
2026-05-29 19:24:09 +0700

    refactor: tambahkan validasi untuk field nama dan harga pada CreateProdukDto

--- API/produk/src/produk/dto/create-produk.dto.ts
+++ API/produk/src/produk/dto/create-produk.dto.ts
@@ -1,4 +1,10 @@
+import { IsString, IsNotEmpty, IsNumber } from 'class-validator';
+
 export class CreateProdukDto {
+  @IsString()
+  @IsNotEmpty()
   nama: string;
+
+  @IsNumber()
   harga: number;
 }

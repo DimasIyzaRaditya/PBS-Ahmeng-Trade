61eb603664420c189f6a4cccd221e56bbdc93cb8
Azizteknokrat <aziz_prayoga@teknokrat.ac.id>
2026-05-29 19:19:27 +0700

    implementasi ValidationPipe untuk validasi global pada aplikasi

--- API/gateway/src/main.ts
+++ API/gateway/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,       // buang field yang tidak ada di DTO
+    forbidNonWhitelisted: false,
+    transform: true,       // otomatis konversi tipe data (string -> number, dll)
+  }));
   await app.listen(3000);
 }
 bootstrap();

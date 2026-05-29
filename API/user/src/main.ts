d686f7a31070d634b64ac22a50ef5a2fded9f967
Azizteknokrat <aziz_prayoga@teknokrat.ac.id>
2026-05-29 19:25:44 +0700

    refactor: tambahkan penggunaan ValidationPipe global untuk validasi dan transformasi data

--- API/user/src/main.ts
+++ API/user/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { UserModule } from './user/user.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(UserModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(3001);
 }
 bootstrap();

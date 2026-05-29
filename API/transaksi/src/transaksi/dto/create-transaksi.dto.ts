ae9bd43a695dea1b6e3565dca9cafc711ac2b548
Azizteknokrat <aziz_prayoga@teknokrat.ac.id>
2026-05-29 19:25:29 +0700

    refactor: perbarui CreateTransaksiDto untuk menyertakan dekorator validasi untuk produkId, namaPembeli, dan emailPembeli

--- API/transaksi/src/transaksi/dto/create-transaksi.dto.ts
+++ API/transaksi/src/transaksi/dto/create-transaksi.dto.ts
@@ -1,6 +1,13 @@
+import { IsString, IsNotEmpty, IsNumber, IsEmail } from 'class-validator';
+
 export class CreateTransaksiDto {
+  @IsNumber()
   produkId: number;
+
+  @IsString()
+  @IsNotEmpty()
   namaPembeli: string;
+
+  @IsEmail()
   emailPembeli: string;
-  totalHarga: number;
 }

fceda3e4c8a9bba43fad9ec5951077753831cedb
Azizteknokrat <aziz_prayoga@teknokrat.ac.id>
2026-05-29 19:20:43 +0700

    menambahkan validasi untuk nama dan harga pada CreateProdukDto

--- API/gateway/src/produk/dto/create-produk.dto.ts
+++ API/gateway/src/produk/dto/create-produk.dto.ts
@@ -1,4 +1,11 @@
+import { IsString, IsNotEmpty, IsNumber } from 'class-validator';
+
 export class CreateProdukDto {
+  @IsString()
+  @IsNotEmpty({ message: 'nama tidak boleh kosong' })
   nama: string;
+
+  @IsNumber({}, { message: 'harga harus berupa angka' })
   harga: number;
 }
+

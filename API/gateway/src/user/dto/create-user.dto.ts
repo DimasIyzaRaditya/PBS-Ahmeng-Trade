0b9728b6ee7e67481ff73fedcc197a378a51e25f
Azizteknokrat <aziz_prayoga@teknokrat.ac.id>
2026-05-29 19:22:11 +0700

    refactor: tambahkan validasi untuk field name, username, dan password pada CreateUserDto

--- API/gateway/src/user/dto/create-user.dto.ts
+++ API/gateway/src/user/dto/create-user.dto.ts
@@ -1,5 +1,16 @@
+import { IsString, IsNotEmpty } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty({ message: 'name tidak boleh kosong' })
   name!: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'username tidak boleh kosong' })
   username!: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'password tidak boleh kosong' })
   password!: string;
 }
+

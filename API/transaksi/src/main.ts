de9793965e99994884c898a62141b9bf8caa4d36
Azizteknokrat <aziz_prayoga@teknokrat.ac.id>
2026-05-29 19:24:40 +0700

    refactor: tambahkan ValidationPipe global dengan whitelist dan transform di main.ts

--- API/transaksi/src/main.ts
+++ API/transaksi/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { TransaksiModule } from './transaksi/transaksi.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(TransaksiModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(3003);
 }
 

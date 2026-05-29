94134d402332506088174e8bf71f92a80efe79eb
Azizteknokrat <aziz_prayoga@teknokrat.ac.id>
2026-05-29 19:19:59 +0700

    menambahkan validasi untuk username dan password pada LoginDto

--- API/gateway/src/auth/dto/login.dto.ts
+++ API/gateway/src/auth/dto/login.dto.ts
@@ -1,4 +1,12 @@
+import { IsString, IsNotEmpty } from 'class-validator';
+
 export class LoginDto {
+  @IsString()
+  @IsNotEmpty({ message: 'username tidak boleh kosong' })
   username!: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'password tidak boleh kosong' })
   password!: string;
 }
+

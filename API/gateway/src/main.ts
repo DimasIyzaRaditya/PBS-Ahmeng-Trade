652cf3e94076c013ed943151a3ec6adb0ebf7752
ridhorr <153862510+ridhorr@users.noreply.github.com>
2026-05-30 19:14:54 +0700

    fix: tambah CORS untuk mobile port 8081 di gateway

--- API/gateway/src/main.ts
+++ API/gateway/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ["http://localhost:3010"],
+    origin: ["http://localhost:3010", "http://localhost:8081"],
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({

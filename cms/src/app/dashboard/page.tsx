0e3bd8f130dff68d995fde44aaf1e8c595e6cecd
DimasIyzaraditya <dimas_iyza_raditya@teknokrat.ac.id>
2026-06-09 12:09:13 +0700

    menambahkan import image

--- cms/src/app/dashboard/page.tsx
+++ cms/src/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 type TabKey = "users" | "produk" | "transaksi";

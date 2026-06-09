18d48d2a064c9c62c33a7b4cef8dcfbc0953d787
DimasIyzaraditya <dimas_iyza_raditya@teknokrat.ac.id>
2026-06-09 12:24:48 +0700

    tambah produkFilter

--- cms/src/app/dashboard/page.tsx
+++ cms/src/app/dashboard/page.tsx
@@ -233,6 +233,7 @@ export default function AdminPanel() {
   const [produkTable, setProdukTable] = useState<PaginationState>({ query: "", page: 1, pageSize: 5 });
   const [transaksiTable, setTransaksiTable] = useState<PaginationState>({ query: "", page: 1, pageSize: 5 });
   const [userFilter, setUserFilter] = useState("all");
+  const [produkFilter, setProdukFilter] = useState("all");
 
   const [userForm, setUserForm] = useState({ id: 0, name: "", username: "", password: "" });
   const [produkForm, setProdukForm] = useState({ id: 0, nama: "", harga: "" });

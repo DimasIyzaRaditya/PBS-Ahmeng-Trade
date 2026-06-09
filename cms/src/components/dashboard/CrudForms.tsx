b1b40ce4f6e7021639434cc844add4a6f2db5960
DimasIyzaraditya <dimas_iyza_raditya@teknokrat.ac.id>
2026-06-09 13:02:51 +0700

    buat ProdukOption

--- cms/src/components/dashboard/CrudForms.tsx
+++ cms/src/components/dashboard/CrudForms.tsx
@@ -2,3 +2,8 @@ import styles from "./dashboard-ui.module.css";
 import { FormField, cx } from "./ui";
 
 type ValidationErrors<T extends string> = Partial<Record<T, string>>;
+
+interface ProdukOption {
+  id: number;
+  nama: string;
+}
